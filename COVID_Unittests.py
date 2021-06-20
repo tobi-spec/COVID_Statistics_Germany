995732e84460fe71b1cdcf709a98300120e7ac81
tobi-spec <66738887+tobi-spec@users.noreply.github.com>
2021-06-20 19:05:05 +0200

    Delete COVID_Unittests.py
    deprecated

--- COVID_Unittests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-# TODO Alle Unittests neu schreiben, 
-# aufgrund der Neustrukturierung sind bisherige Unittests unbrauchbar geworden
-
-if __name__ == '__main__':
-
-    unittest.main()
