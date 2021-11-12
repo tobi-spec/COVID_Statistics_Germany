02c5a255d039b36c0b0f38c3d63e0880e4206511
Tobias Weiland <tobias.weiland@7p-group.com>
2021-11-12 19:49:36 +0100

    create fetch rki and fetch divi route in controller

--- backend/FetchController.py
+++ backend/FetchController.py
@@ -1,8 +1,21 @@
 from flask import Flask
 
+import DIVIService
+import RKIService
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def get_connection():
     return 'Hello, World! This server works fine!'
 
+
+@app.route("/fetch/rki")
+def fetch_rki():
+    return RKIService.fetch()
+
+
+@app.route("/fetch/divi")
+def fetch_divi():
+    return DIVIService.fetch()
