261c184f850fc3cee5be87db121b27b2649c1c9c
tobi8691 <post@tobias-hahn.eu>
2022-01-23 13:45:46 +0100

    bring back running  rki data fetcher

--- src/FetchRKIService.py
+++ src/FetchRKIService.py
@@ -6,14 +6,12 @@ from pathlib import Path
 This class enables the download and handle of COVID-19 epidemiological data provides by the german RKI Institut (www.RKI.de)
 All data is gathereed into a single csv file, which makes the file very large (> 1.8million columns)
 
-RKI.fetch allows the download of the whole file, saved in a seperated folder (./data/rki)
-
 '''
 
 
 def fetch():
     url = "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
-    directory = Path("data/rki")
+    directory = Path("./data/raw/rki.csv")
     print("this will need a while...")
     save_csv(directory, url)
     print("Finished!")
